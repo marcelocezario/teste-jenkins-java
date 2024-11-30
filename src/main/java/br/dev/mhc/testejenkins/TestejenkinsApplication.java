3f1f9d2a765fc52e8547f532dc5444bc3e4e22e3
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-11-30 09:53:41 -0300

    Initial commit

---
+++ src/main/java/br/dev/mhc/testejenkins/TestejenkinsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.testejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestejenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestejenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/mhc/testejenkins/TestejenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.testejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestejenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
